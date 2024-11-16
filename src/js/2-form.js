f4adf9ce0bd51cabf7b0211a3e8624ee2eb790c7
Lesia_Nazaruk <nazarullesia@gmail.com>
2024-11-16 21:10:12 +0200

    add form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,55 @@
-console.log("Form");
+
+const formData = {
+  email: "",
+  message: "",
+};
+
+const form = document.querySelector('.feedback-form');
+
+console.log(form);
+
+const fillData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (fillData) {
+  formData.email = fillData.email || "";
+    formData.message = fillData.message || "";
+    
+     form.email.value = formData.email;
+    form.message.value = formData.message;
+}
+    
+form.addEventListener('input', (event) => {
+  const { name, value } = event.target;
+
+  if (name === 'email') {
+    formData.email = value.trim();
+  }
+
+  if (name === 'message') {
+    formData.message = value.trim();
+  }
+
+  
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+ 
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  
+console.log(formData);
+  
+
+  localStorage.removeItem('feedback-form-state');
+  formData.email = "";
+  formData.message = "";
+  form.email.value = "";
+  form.message.value = "";
+});
